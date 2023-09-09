32ff3aded50f2799bbcee0b0211511f1c646f14d
jabss <jabrijuhinin93@gmail.com>
2023-09-09 12:13:32 +0800

    Include objects in content

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,11 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
 
   const Header = (props) => {
     return (
@@ -26,9 +40,9 @@ const App = () => {
   const Content = (props) => {
     return (
       <div>
-        <Part part={part1} exercises={exercises1} />
-        <Part part={part2} exercises={exercises2} />
-        <Part part={part3} exercises={exercises3} />
+        <Part part={part1.name} exercises={part1.exercises} />
+        <Part part={part2.name} exercises={part2.exercises} />
+        <Part part={part3.name} exercises={part3.exercises} />
       </div>
     )
   }
@@ -45,7 +59,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content />
-      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
+      <Total exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />
     </div>
   )
 }
